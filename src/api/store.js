412c4a255390722bf428dcffdd9f9ccff632dc90
yzm17339637110 <yzm19991127@163.com>
2022-07-06 10:15:55 +0800

    店铺界面建立

---
+++ src/api/store.js
@@ -0,0 +1,19 @@
+import request from "@/utils/ajax.js";
+
+/* 获取店铺信息 */
+export function getInfo(data) {
+    return request({
+        url:"/store/getInfo",
+        method:"post",
+        data
+    })
+}
+
+/* 获取店铺信息 */
+export function getInfo(data) {
+    return request({
+        url:"/store/getInfo",
+        method:"post",
+        data
+    })
+}
