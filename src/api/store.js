e07b8f04b6b7aa525df6258838c261a3d70fd199
yzm17339637110 <yzm19991127@163.com>
2022-07-06 11:21:01 +0800

    feat:店铺页面完成

--- src/api/store.js
+++ src/api/store.js
@@ -9,11 +9,11 @@ export function getInfo(data) {
     })
 }
 
-/* 获取店铺信息 */
-export function getInfo(data) {
-    return request({
-        url:"/store/getInfo",
-        method:"post",
-        data
-    })
-}
+/* 更改店铺信息 */
+// export function getInfo(data) {
+//     return request({
+//         url:"/store/getInfo",
+//         method:"post",
+//         data
+//     })
+// }
